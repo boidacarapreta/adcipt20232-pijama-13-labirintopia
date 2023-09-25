dba7f6db59b462d09168c7f130b212aa9cea4bf8
anasilvachneider <ana18beatrizsilvajjj@gmail.com>
2023-09-25 19:53:31 +0000

    ajustes

--- cliente/js/cenas/cena-sala.js
+++ cliente/js/cenas/cena-sala.js
@@ -30,79 +30,79 @@ export default class sala extends Phaser.Scene {
 
     this.imagem = this.add.image(400, 225, 'salas').setTint(0xcccccc)
 
-  }
-   this.salas = [
 
-  {
-    numero: '1',
-    x: 135,
-    y: 195,
-    botao: undefined
-  },
-  {
-    numero: '2',
-    x: 282,
-    y: 195,
-    botao: undefined
-  },
-  {
-    numero: '3',
-    x: 514,
-    y: 195,
-    botao: undefined
-  },
-  {
-    numero: '4',
-    x: 672,
-    y: 195,
-    botao: undefined
-  },
-  {
-    numero: '5',
-    x: 217,
-    y: 295,
-    botao: undefined
-  },
-  {
-    numero: '6',
-    x: 400,
-    y: 295,
-    botao: undefined
-  },
-  {
-    numero: '7',
-    x: 593,
-    y: 295,
-    botao: undefined
-  },
-  {
-    numero: '8',
-    x: 318,
-    y: 359,
-    botao: undefined
-  },
-  {
-    numero: '9',
-    x: 505,
-    y: 359,
-    botao: undefined
-  },
-  {
-    numero: '10',
-    x: 394,
-    y: 425,
-    botao: undefined
-  }
-]
+    this.salas = [
 
-this.salas.forEach((item) => {
-  item.botao = this.add
-    .sprite(item.x, item.y, 'vazio')
-    .setInteractive()
-    .on('pointerdown', () => {
-      this.game.scene.stop('sala')
-      this.game.scene.start('principal')
+      {
+        numero: '1',
+        x: 135,
+        y: 195,
+        botao: undefined
+      },
+      {
+        numero: '2',
+        x: 282,
+        y: 195,
+        botao: undefined
+      },
+      {
+        numero: '3',
+        x: 514,
+        y: 195,
+        botao: undefined
+      },
+      {
+        numero: '4',
+        x: 672,
+        y: 195,
+        botao: undefined
+      },
+      {
+        numero: '5',
+        x: 217,
+        y: 295,
+        botao: undefined
+      },
+      {
+        numero: '6',
+        x: 400,
+        y: 295,
+        botao: undefined
+      },
+      {
+        numero: '7',
+        x: 593,
+        y: 295,
+        botao: undefined
+      },
+      {
+        numero: '8',
+        x: 318,
+        y: 359,
+        botao: undefined
+      },
+      {
+        numero: '9',
+        x: 505,
+        y: 359,
+        botao: undefined
+      },
+      {
+        numero: '10',
+        x: 394,
+        y: 425,
+        botao: undefined
+      }
+    ]
+
+    this.salas.forEach((item) => {
+      item.botao = this.add
+        .sprite(item.x, item.y, 'vazio')
+        .setInteractive()
+        .on('pointerdown', () => {
+          this.game.scene.stop('sala')
+          this.game.scene.start('principal')
+        })
     })
-})
   }
-
+}
