058b4a8257b4f477483e996a13c1062739697e53
adr franciny <franciny.r11@aluno.ifsc.edu.br>
2023-09-29 20:37:19 +0000

    atualização

--- cliente/js/cenas/cena-principal.js
+++ cliente/js/cenas/cena-principal.js
@@ -12,6 +12,13 @@ export default class principal extends Phaser.Scene {
     this.load.image('parede-vertical', '../assets/mapa/parede-vertical.png')
     this.load.image('topo-de-quina', '../assets/mapa/topo-de-quina.png')
     this.load.image('base-quina', '../assets/mapa/base-quina.png')
+    this.load.image('sombra1', '../assets/mapa/sombra1.png')
+    this.load.image('sombra2', '../assets/mapa/sombra2.png')
+    this.load.image('sombra3', '../assets/mapa/sombra3.png')
+    this.load.image('sombra4', '../assets/mapa/sombra4.png')
+    this.load.image('sombra5', '../assets/mapa/sombra5.png')
+    this.load.image('sombra6', '../assets/mapa/sombra6.png')
+    this.load.image('sombra7', '../assets/mapa/sombra7.png')
   }
 
   create () { 
@@ -25,10 +32,18 @@ export default class principal extends Phaser.Scene {
     this.tilesetParedeVertical = this.tilemapLabirinto.addTilesetImage('parede-vertical')
     this.tilesetTopoDeQuina = this.tilemapLabirinto.addTilesetImage('topo-de-quina')
     this.tilesetBaseQuina = this.tilemapLabirinto.addTilesetImage('base-quina')
+    this.tilesetSombra1 = this.tilemapLabirinto.addTilesetImage('sombra1')
+    this.tilesetSombra2 = this.tilemapLabirinto.addTilesetImage('sombra2')
+    this.tilesetSombra3 = this.tilemapLabirinto.addTilesetImage('sombra3')
+    this.tilesetSombra4 = this.tilemapLabirinto.addTilesetImage('sombra4')
+    this.tilesetSombra5 = this.tilemapLabirinto.addTilesetImage('sombra5')
+    this.tilesetSombra6 = this.tilemapLabirinto.addTilesetImage('sombra6')
+    this.tilesetSombra7 = this.tilemapLabirinto.addTilesetImage('sombra7')
 
     this.layerTerreno = this.tilemapLabirinto.createLayer('terreno', [this.tilesetTerreno])
     this.layerParede = this.tilemapLabirinto.createLayer('parede', [this.tilesetBaseParede, this.tilesetParedeHorizontal, this.tilesetParedeVertical, this.tilesetTopoDeQuina, this.tilesetBaseQuina])
-  }
+     this.layerSombra = tis.tilemapLabirinto.createLayer('sombra', [this.tilesetSombra1, this.tilesetSombra2, this.tilesetSombra3, this.tilesetSombra4, this.tilesetSombra5, this.tilesetSombra6, this.tilesetSombra7])
 
+  }
   update () { }
 }
