bb79628a77f4068b74ae765987a065875294693a
anasilvachneider <ana18beatrizsilvajjj@gmail.com>
2023-10-03 14:00:11 +0000

    ajustes

--- cliente/js/cenas/cena-principal.js
+++ cliente/js/cenas/cena-principal.js
@@ -6,6 +6,11 @@ export default class principal extends Phaser.Scene {
   preload () {
     this.load.tilemapTiledJSON('labirinto', '../assets/mapa/mapa.json')
 
+    this.load.image('base-parede1', '../assets/mapa/base-parede.1.png')
+    this.load.image('parede-horizontal1', '../assets/mapa/parede-horizontal.1.png')
+    this.load.image('parede-vertical1', '../assets/mapa/parede-vertical.1.png')
+    this.load.image('topo-de-quina1', '../assets/mapa/topo-de-quina.1.png')
+    this.load.image('base-quina1', '../assets/mapa/base-quina.1.png')
     this.load.image('sombra1', '../assets/mapa/sombra1.png')
     this.load.image('sombra2', '../assets/mapa/sombra2.png')
     this.load.image('sombra3', '../assets/mapa/sombra3.png')
@@ -14,19 +19,33 @@ export default class principal extends Phaser.Scene {
     this.load.image('sombra6', '../assets/mapa/sombra6.png')
     this.load.image('sombra7', '../assets/mapa/sombra7.png')
     this.load.image('terreno', '../assets/mapa/terreno.1.png')
-    this.load.image('base-parede1', '../assets/mapa/base-parede.1.png')
-    this.load.image('parede-horizontal1', '../assets/mapa/parede-horizontal.1.png')
-    this.load.image('parede-vertical1', '../assets/mapa/parede-vertical.1.png')
-    this.load.image('topo-de-quina1', '../assets/mapa/topo-de-quina.1.png')
-    this.load.image('base-quina1', '../assets/mapa/base-quina.1.png')
 
-    this.load.spritesheet('botao', '../assets/botoes/botoes.png', {
+    this.load.spritesheet('azul', '../assets/mapa/azul.png', {
+      frameWidth: 64,
+      frameHeight: 128
+    })
+    
+    this.load.spritesheet('esquerda', '../assets/esquerda.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+    this.load.spritesheet('direita', '../assets/botoes/direita.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+    this.load.spritesheet('cima', '../assets/botoes/cima.png', {
+      frameWidth: 64,
+      frameHeight: 64
+    })
+    this.load.spritesheet('baixo', '../assets/botoes/baixo.png', {
       frameWidth: 64,
       frameHeight: 64
     })
   }
 
   create () {
+
+    /*mapa*/
     this.tilemapLabirinto = this.make.tilemap({
       key: 'labirinto'
     })
@@ -48,6 +67,114 @@ export default class principal extends Phaser.Scene {
     this.layerTerreno = this.tilemapLabirinto.createLayer('terreno', [this.tilesetTerreno1])
     this.layerParede = this.tilemapLabirinto.createLayer('parede', [this.tilesetBaseParede1, this.tilesetParedeHorizontal1, this.tilesetParedeVertical1, this.tilesetTopoDeQuina1, this.tilesetTopoDeQuina1])
     this.layerSombra = this.tilemapLabirinto.createLayer('sombra', [this.tilesetSombra1, this.tilesetSombra2, this.tilesetSombra3, this.tilesetSombra4, this.tilesetSombra5, this.tilesetSombra6, this.tilesetSombra7])
+  
+    /*personagens*/
+
+    this.personagem = this.physics.add.sprite(-350, -80, 'azul', 18)
+    this.cameras.main.startFollow(this.personagem)
+
+    this.anims.create({
+      key: 'azul-parado',
+      frames: this.anims.generateFrameNumbers('azul', {
+        start: 18,
+        end: 18
+      }),
+      frameRate: 1
+    })
+    this.anims.create({
+      key: 'azul-esquerda',
+      frames: this.anims.generateFrameNumbers('azul', {
+        start: 9,
+        end: 17
+      }), frameRate: 12,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'azul-direita',
+      frames: this.anims.generateFrameNumbers('azul', {
+        start: 27,
+        end: 35
+      }), frameRate: 12,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'azul-cima',
+      frames: this.anims.generateFrameNumbers('azul', {
+        start: 0,
+        end: 8
+      }),
+      frameRate: 12,
+      repeat: -1
+    })
+    this.anims.create({
+      key: 'azul-baixo',
+      frames: this.anims.generateFrameNumbers('azul', {
+        start: 18,
+        end: 26
+      }),
+      frameRate: 12,
+      repeat: -1
+    })
+ 
+
+    /*botões*/
+    this.esquerda = this.add.sprite(50, 350, 'esquerda')
+      .setScrollFactor(0)
+      .setInteractive()
+      .on('pointerover', () => {
+        this.esquerda.setFrame(1)
+        this.personagem.anims.play('azul-esquerda')
+        this.personagem.setVelocityX(this.velocidade)
+      })
+      .on('pointerout', () => {
+        this.esquerda.setFrame(0)
+        this.personagem.anims.play('azul-parado')
+        this.personagem.setVelocityX(0)
+      })
+    this.direita = this.add.sprite(150, 350, 'direita')
+      .setScrollFactor(0)
+      .setInteractive()
+      .on('pointerover', () => {
+        this.direita.setFrame(1)
+        this.personagem.anims.play('azul-direita')
+        this.personagem.setVelocityX(this.velocidade)
+      })
+      .on('pointerout', () => {
+        this.direita.setFrame(0)
+        this.personagem.anims.play('azul-parado')
+        this.personagem.setVelocityX(0)
+      })
+    this.cima = this.add.sprite(100, 300, 'cima')
+      .setScrollFactor(0)
+      .setInteractive()
+      .on('pointerover', () => {
+        this.cima.setFrame(1)
+        this.personagem.anims.play('azul-cima')
+        this.personagem.setVelocityY(-this.velocidade)
+      })
+      .on('pointerout', () => {
+        this.cima.setFrame(0)
+        this.personagem.anims.play('azul-parado')
+        this.personagem.setVelocityY(0)
+      })
+    this.baixo = this.add.sprite(100, 400, 'baixo')
+      .setScrollFactor(0)
+      .setInteractive()
+      .on('pointerover', () => {
+        this.baixo.setFrame(1)
+        this.personagem.anims.play('azul-baixo')
+        this.personagem.setVelocityY(this.velocidade)
+      })
+      .on('pointerout', () => {
+        this.baixo.setFrame(0)
+        this.personagem.anims.play('azul-parado')
+        this.personagem.setVelocityY(0)
+      })
+  
+  
+  
+  
+  
   }
 
   update () { }
