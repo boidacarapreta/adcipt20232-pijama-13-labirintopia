192bbc2b139aa0658b8d7925e6a76608d965b59a
anasilvachneider <ana18beatrizsilvajjj@gmail.com>
2023-11-30 20:07:49 +0000

    tentativa de criação de desafio

--- cliente/js/cenas/desafiofinal.js
+++ cliente/js/cenas/desafiofinal.js
@@ -5,11 +5,24 @@ export default class desafiofinal extends Phaser.Scene {
 
   preload () {
     this.load.image('escolhas', './assets/escolhas.png')
+    this.load.image('gradedesistir', '../assets/botoes/gradedesistir.png')
+    this.load.image('gradecontinuar', '../assets/botoes/gradecontinuar.png')
   }
 
   create () {
     this.imagem = this.add.image(400, 225, 'escolhas')
-    
+    this.image = this.add.image(400, 408, 'gradecontinuar')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.game.scene.stop('desafiofinal')
+        this.game.scene.resume('principal')
+      })
+    this.image = this.add.physics.image(200, 408, 'gradedesistir')
+      .setInteractive()
+      .on('pointerdown', () => {
+        this.game.scene.stop('desafiofinal')
+        this.game.scene.start('finaltriste')
+      })
   }
 
   update () { }
